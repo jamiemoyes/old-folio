22c712ca3facd80ac8a6660d91b426285044efab
jamiemoyes <49124284+jamiemoyes@users.noreply.github.com>
2019-11-17 14:30:19 +0000

    testing js

--- script.js
+++ script.js
@@ -16,23 +16,6 @@ circularText("jamie moyes ", 100, 0);
 $(window).on("load",function() {
     $(window).scroll(function() {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $("#secondaryText").each(function() {
-            /* Check the location of each desired element */
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-            } else { //object goes out of view (scrolling up)
-                if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-            }
-        });
-    }).scroll(); //invoke scroll-handler on page-load
-});
-
-$(window).on("load",function() {
-    $(window).scroll(function() {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
         $("#secondaryText2").each(function() {
             /* Check the location of each desired element */
             var objectBottom = $(this).offset().top + $(this).outerHeight();
